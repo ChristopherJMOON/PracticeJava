1c2e8b53731faa6b9b3259bc1ea1690891690d56
Christopher Mooney <chrislinked052@gmail.com>
2021-09-28 21:47:42 +0100

    Test.java has had an extra for loop added for testing

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,17 @@ public class Test{
     public static void main(String[] args){
 
         System.out.print("This is a test file for git repo");
+
+        int count = 6;
+
+        for(int i = 0; i < count; i++){
+            System.out.println("\nThis was created using a for loop");
+        }
+
+
+        for(int i = 0; i < count; i++){
+            System.out.println("This is a new line of code created " +
+                    "for git hub test");
+        }
     }
 }
